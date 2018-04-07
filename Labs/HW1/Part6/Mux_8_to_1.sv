515a990ae1ea7b633f46f4b881fb1ad3f840a9d3
jakemckenzie <jake314@uw.edu>
2018-04-07 13:32:14 -0700

    More part 6

--- Labs/HW1/Part6/Mux_8_to_1.sv
+++ Labs/HW1/Part6/Mux_8_to_1.sv
@@ -1,13 +1,39 @@
-//a[0] = select bit 0     a[1] = select bit 1     a[2] = select bit 2
-//a[11:3] = wires
-module Mux_8_to_1(input [11:0]a, output z);
-    assign w0 = a[0] ? a[4] : a[3];
-    assign w1 = a[0] ? a[7] : a[6];
-    assign w2 = a[0] ? a[9] : a[8];
-    assign w3 = a[0] ? a[11] : a[10];
+module Mux_8_to_1(input [7:0]mux_input,[2:0]select_bits, output z);
+    assign wire_0 = select_bits[0] ? mux_input[4] : mux_input[3];
+    assign wire_1 = select_bits[0] ? mux_input[7] : mux_input[6];
+    assign wire_2 = select_bits[0] ? mux_input[9] : mux_input[8];
+    assign wire_3 = select_bits[0] ? mux_input[11] : mux_input[10];
 
-    assign w4 = a[1] ? w1 : w0;
-    assign w5 = a[1] ? w3 : w2;
+    assign wire_4 = select_bits[1] ? wire_1 : wire_0;
+    assign wire_5 = select_bits[1] ? wire_3 : wire_2;
 
-    assign z = a[2] ? w5 : w4;
+    assign z = a[2] ? wire_5 : wire_4;
 endmodule
+
+module Mux_8_to_1_testbench();
+    reg []select_bits
+    reg [7:0]mux_input;
+    wire z;
+ 
+    Mux_8_to_1 DUT(mux_input[7:0],select_bits[2:0]);
+    initial begin
+        //mux_input[0] = 0;mux_input[1] = 0;mux_input[2] = 0;mux_input[3] = 0;
+        //mux_input[4] = 0;mux_input[5] = 0;mux_input[6] = 0;mux_input[7] = 0;
+        //https://stackoverflow.com/questions/26207021/how-to-write-a-verilog-testbench-to-loop-through-4-inputs
+        always @ (*) begin
+            for (int i = 0; i < 8; i++) begin
+                mux_input[k] = 0;
+            end
+        end
+
+        $monitor(mux_input[7:0]);
+        always @(*)
+            for (int i = 0; i < 8; i++) begin
+                mux_input[7:0] = i;
+                #10;
+            end
+        end
+        
+    end
+ 
+ endmodule // Mux_5_to_1_testbench
