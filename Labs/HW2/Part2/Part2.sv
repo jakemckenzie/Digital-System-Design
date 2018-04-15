bf2158b14a4fd8d21f234eb44b45c227f462565d
jakemckenzie <jake314@uw.edu>
2018-04-15 16:32:40 -0700

    testbench outputting wrong values, probably need to fix the cases

--- Labs/HW2/Part2/Part2.sv
+++ Labs/HW2/Part2/Part2.sv
@@ -6,70 +6,87 @@ module Part2(input [3:0]SW, output logic [0:6]HEX1,HEX0, output [3:0]LEDG);
     always_comb//https://stackoverflow.com/questions/47735552/updating-multiple-variables-in-case-statement
     begin
         case(SW)//input [3:0]foo, output [0:6]lambda
-            4'h0: begin
+            4'h0: 
+            begin
                 psi = 4'h0;
                 phi = 4'h0;//0
             end
-            4'h1: begin
+            4'h1: 
+            begin
                 psi = 4'h0;
                 phi = 4'h1;//1
             end
-            4'h2: begin
+            4'h2: 
+            begin
                 psi = 4'h0;
                 phi = 4'h2;//2
             end
-            4'h3: begin
+            4'h3: 
+            begin
                 psi = 4'h0;
                 phi = 4'h3;//3
             end
-            4'h4: begin
+            4'h4: 
+            begin
                 psi = 4'h0;
                 phi = 4'h4;//4
             end
-            4'h5: begin
+            4'h5: 
+            begin
                 psi = 4'h0;
                 phi = 4'h5;//5
             end
-            4'h6: begin
+            4'h6: 
+            begin
                 psi = 4'h0;
                 phi = 4'h6;//6
             end
-            4'h7: begin
+            4'h7: 
+            begin
                 psi = 4'h0;
                 phi = 4'h7;//7
             end
-            4'h8: begin
+            4'h8: 
+            begin
                 psi = 4'h0;
                 phi = 4'h8;//8
             end
-            4'h9: begin
+            4'h9: 
+            begin
                 psi = 4'h0;
                 phi = 4'h9;//9
             end
-            4'hA: begin
+            4'hA: 
+            begin
                 psi = 4'h1;
                 phi = 4'h0;//10
             end
-            4'hB: begin
+            4'hB: 
+            begin
                 psi = 4'h1;
                 phi = 4'h1;//11
             end
-            4'hC: begin
+            4'hC: 
+            begin
                 psi = 4'h1;
                 phi = 4'h2;//12
             end
-            4'hD: begin
+            4'hD: 
+            begin
                 psi = 4'h1;
                 phi = 4'h3;//13
             end
-            4'hE: begin
+            4'hE: 
+            begin
                 psi = 4'h1;
                 phi = 4'h4;//14
             end
-            4'hF: begin
+            4'hF: 
+            begin
                 psi = 4'h1;
                 phi = 4'h5;//15
             end
+            //default: psi,phi 4'hF;//default signal is no display
         endcase
     end
 endmodule
@@ -88,7 +105,7 @@ module Part2_testbench();
     initial
     begin
         for(i = 0;i < 16;i++) 
-        begin 
+        begin
             $monitor("%h        %h      %h      %h",SW,HEX1,HEX0,LEDG);
             SW = i;#10;
             assert(SW == LEDG);
