59b938223e4aba0a13e4129fa358584633a86179
jakemckenzie <jake314@uw.edu>
2018-04-14 23:54:12 -0700

    part 2 hw 2 complete

--- Labs/HW2/Part2/Part2.sv
+++ Labs/HW2/Part2/Part2.sv
@@ -1,23 +1,26 @@
 module Part2(input [3:0]SW, output [0:6]HEX1,HEX0, output [3:0]LEDG);
-    LEDG = SW;
-
+    assign LEDG = SW;
+	wire = [3:0]psi,phi;
+    Decoder_part2 Decoder_part2_1(psi[3:0],HEX1[0:6]); Decoder_part2 Decoder_part2_2(phi[3:0],HEX0[0:6]);
+    
     always_comb
     begin
         case(SW)//input [3:0]foo, output [0:6]lambda
-            4'h0: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h0,HEX0[0:6]);//0
-            4'h1: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h1,HEX0[0:6]);//1
-            4'h2: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h2,HEX0[0:6]);//2
-            4'h3: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h3,HEX0[0:6]);//3
-            4'h4: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h4,HEX0[0:6]);//4
-            4'h5: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h5,HEX0[0:6]);//5
-            4'h6: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h6,HEX0[0:6]);//6
-            4'h7: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h7,HEX0[0:6]);//7
-            4'h8: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h8,HEX0[0:6]);//8
-            4'h9: Decoder_part2 Decoder_part2_1(4'h0,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h9,HEX0[0:6]);//9
-            4'hA: Decoder_part2 Decoder_part2_1(4'h1,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h0,HEX0[0:6]);//10
-            4'hB: Decoder_part2 Decoder_part2_1(4'h1,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h1,HEX0[0:6]);//11
-            4'hC: Decoder_part2 Decoder_part2_1(4'h1,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h2,HEX0[0:6]);//12
-            4'hD: Decoder_part2 Decoder_part2_1(4'h1,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h3,HEX0[0:6]);//13
-            4'hE: Decoder_part2 Decoder_part2_1(4'h1,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h4,HEX0[0:6]);//14
-            4'hF: Decoder_part2 Decoder_part2_1(4'h1,HEX1[0:6]); Decoder_part2 Decoder_part2_2(4'h5,HEX0[0:6]);//15
+            4'h0: psi = 4'h0;phi = 4'h0;    //0
+            4'h1: psi = 4'h0;phi = 4'h1;    //1
+            4'h2: psi = 4'h0;phi = 4'h2;    //2
+            4'h3: psi = 4'h0;phi = 4'h3;    //3
+            4'h4: psi = 4'h0;phi = 4'h4;    //4
+            4'h5: psi = 4'h0;phi = 4'h5;    //5
+            4'h6: psi = 4'h0;phi = 4'h6;    //6
+            4'h7: psi = 4'h0;phi = 4'h7;    //7
+            4'h8: psi = 4'h0;phi = 4'h8;    //8
+            4'h9: psi = 4'h0;phi = 4'h9;    //9
+            4'hA: psi = 4'h1;phi = 4'h0;    //10
+            4'hB: psi = 4'h1;phi = 4'h1;    //11
+            4'hC: psi = 4'h1;phi = 4'h2;    //12
+            4'hD: psi = 4'h1;phi = 4'h3;    //13
+            4'hE: psi = 4'h1;phi = 4'h4;    //14
+            4'hF: psi = 4'h1;phi = 4'h5;    //15
+            default psi = 4'hF;phi = 4'hF;  //default signal is no display
 endmodule
