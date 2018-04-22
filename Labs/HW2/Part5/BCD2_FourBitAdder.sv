8e38a5cb52fa1fc83f7683a4dfca7a12f6d19f78
jakemckenzie <jake314@uw.edu>
2018-04-21 17:15:23 -0700

    Part 5 done without testbench

--- Labs/HW2/Part5/BCD2_FourBitAdder.sv
+++ Labs/HW2/Part5/BCD2_FourBitAdder.sv
@@ -3,8 +3,8 @@ module BCD2_FourBitAdder(input logic ci,input logic [3:0]vec_in_b,vec_in_a, outp
 	//logic [3:0]S_out;
 	logic [2:0]vec_c_out;
 	//input logic ci,b,a, output logic s,c0
-	BCD_Adder FA1(ci,vec_in_b[0],vec_in_a[0],vec_out_s[0],vec_c_out[0]);
-	BCD_Adder FA2(vec_c_out[0],vec_in_b[1],vec_in_a[1],vec_out_s[1],vec_c_out[1]);
-	BCD_Adder FA3(vec_c_out[1],vec_in_b[2],vec_in_a[2],vec_out_s[2],vec_c_out[2]);
-	BCD_Adder FA4(vec_c_out[2],vec_in_b[3],vec_in_a[3],vec_out_s[3],c_out);
+	BCD2_Adder FA1(ci,vec_in_b[0],vec_in_a[0],vec_out_s[0],vec_c_out[0]);
+	BCD2_Adder FA2(vec_c_out[0],vec_in_b[1],vec_in_a[1],vec_out_s[1],vec_c_out[1]);
+	BCD2_Adder FA3(vec_c_out[1],vec_in_b[2],vec_in_a[2],vec_out_s[2],vec_c_out[2]);
+	BCD2_Adder FA4(vec_c_out[2],vec_in_b[3],vec_in_a[3],vec_out_s[3],c_out);
 endmodule
--- Labs/HW2/Part5/Part5.sv
+++ Labs/HW2/Part5/Part5.sv
@@ -1,25 +1,27 @@
-module Part5(input logic [16:0]SW,output logic [0:6]HEX7,HEX6,HEX5,HEX4,HEX1,HEX0,output logic [16:0]LEDR,output logic [8:8]LEDG);
+module Part5(input logic [16:0]SW,output logic [0:6]HEX7,HEX6,HEX5,HEX4,HEX2,HEX1,HEX0,output logic [16:0]LEDR,output logic [8:8]LEDG);
     logic [3:0]vec_to_HEX4,vec_to_HEX6;
     logic ci;
 	logic [8:0]vec_to_decimal;
 	//logic [8:0]vec_to_display;
 	assign LEDR = SW;
-    BCD_FourBitAdder BCD_FBA1(SW[16],SW[3:0],SW[7:4],vec_to_decimal[3:0],ci);
-    BCD_FourBitAdder BCD_FBA1(ci,SW[15:12],SW[11:8],vec_to_decimal[7:4],vec_to_decimal[8]);
+    BCD2_FourBitAdder BCD2_FBA1(SW[16],SW[3:0],SW[7:4],vec_to_decimal[3:0],ci);
+    BCD2_FourBitAdder BCD2_FBA2(ci,SW[11:8],SW[15:12],vec_to_decimal[7:4],vec_to_decimal[8]);
 	//BCD_Decoder Decoder_for_display0(vec_to_s,HEX0);
 	//BCD_Decoder Decoder_for_display1(vec_to_c,HEX1);
-	//BCD_Decoder_Decimal Decoder_for_display(vec_to_decimal,HEX2,HEX1,HEX0);
-    BCD_Decoder Decoder_for_display4(vec_to_decimal[3:0],HEX0);
-    BCD_Decoder Decoder_for_display4(vec_to_decimal[7:4],HEX1);
-    BCD_Decoder Decoder_for_display4(vec_to_decimal[11:8],HEX2);
-    BCD_Decoder Decoder_for_display4(SW[3:0],HEX4);
-    BCD_Decoder Decoder_for_display4(SW[7:4],HEX5);
-    BCD_Decoder Decoder_for_display4(SW[11:8],HEX6);
-	BCD_Decoder Decoder_for_display6(SW[15:12],HEX7);
+	BCD2_Decoder_Decimal Decoder2_for_display(vec_to_decimal,HEX2,HEX1,HEX0);
+    //BCD_Decoder Decoder_for_display4(vec_to_decimal[3:0],HEX0);
+    //BCD_Decoder Decoder_for_display4(vec_to_decimal[7:4],HEX1);
+    //BCD_Decoder Decoder_for_display4(vec_to_decimal[11:8],HEX2);
+    BCD2_Decoder Decoder2_for_display4(SW[3:0],HEX4);
+    BCD2_Decoder Decoder2_for_display5(SW[7:4],HEX5);
+    BCD2_Decoder Decoder2_for_display6(SW[11:8],HEX6);
+	BCD2_Decoder Decoder2_for_display7(SW[15:12],HEX7);
 	always_comb
 	begin
 		if(SW[7:4] > 4'h8) LEDG[8] = 1'b1;
 		else if(SW[3:0] > 4'h8) LEDG[8] = 1'b1;
+		else if(SW[11:8] > 4'h8) LEDG[8] = 1'b1;
+		else if(SW[15:12] > 4'h8) LEDG[8] = 1'b1;
 		else LEDG[8] = 1'b0;
 	end
 endmodule
