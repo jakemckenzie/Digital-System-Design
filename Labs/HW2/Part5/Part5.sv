6867802b0cc6f59cc3a060f0401ab77101353a02
jakemckenzie <jake314@uw.edu>
2018-04-23 21:49:01 -0700

    part 5 fixed. Wasn't outputting hex display properly

--- Labs/HW2/Part5/Part5.sv
+++ Labs/HW2/Part5/Part5.sv
@@ -4,8 +4,8 @@ module Part5(input logic [16:0]SW,output logic [0:6]HEX7,HEX6,HEX5,HEX4,HEX2,HEX
 	logic [8:0]vec_to_decimal;
 	//logic [8:0]vec_to_display;
 	assign LEDR = SW;
-    BCD2_FourBitAdder BCD2_FBA1(SW[16],SW[3:0],SW[7:4],vec_to_decimal[3:0],ci);
-    BCD2_FourBitAdder BCD2_FBA2(ci,SW[11:8],SW[15:12],vec_to_decimal[7:4],vec_to_decimal[8]);
+    BCD2_FourBitAdder BCD2_FBA1(SW[16],SW[3:0],SW[11:8],vec_to_decimal[3:0],ci);
+    BCD2_FourBitAdder BCD2_FBA2(ci,SW[7:4],SW[15:12],vec_to_decimal[7:4],vec_to_decimal[8]);
 	//BCD_Decoder Decoder_for_display0(vec_to_s,HEX0);
 	//BCD_Decoder Decoder_for_display1(vec_to_c,HEX1);
 	BCD2_Decoder_Decimal Decoder2_for_display(vec_to_decimal,HEX2,HEX1,HEX0);
@@ -13,8 +13,8 @@ module Part5(input logic [16:0]SW,output logic [0:6]HEX7,HEX6,HEX5,HEX4,HEX2,HEX
     //BCD_Decoder Decoder_for_display4(vec_to_decimal[7:4],HEX1);
     //BCD_Decoder Decoder_for_display4(vec_to_decimal[11:8],HEX2);
     BCD2_Decoder Decoder2_for_display4(SW[3:0],HEX4);
-    BCD2_Decoder Decoder2_for_display5(SW[11:8],HEX5);
-    BCD2_Decoder Decoder2_for_display6(SW[7:4],HEX6);
+    BCD2_Decoder Decoder2_for_display5(SW[7:4],HEX5);
+    BCD2_Decoder Decoder2_for_display6(SW[11:8],HEX6);
 	BCD2_Decoder Decoder2_for_display7(SW[15:12],HEX7);
 	always_comb
 	begin
