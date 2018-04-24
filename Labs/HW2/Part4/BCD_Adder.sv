5c35b6508fc57179c5a19dbfe81f2d2372206e56
jakemckenzie <jake314@uw.edu>
2018-04-23 23:20:44 -0700

    part 4 testbench mostly done

--- Labs/HW2/Part4/BCD_Adder.sv
+++ Labs/HW2/Part4/BCD_Adder.sv
@@ -18,15 +18,15 @@ module BCD_Adder_testbench();
         b = 0;a = 0;ci = 0;c0 = 0;s = 0;
         for(i = 0; i < 2; i++)
         begin
-            $monitor("%b%b%b       %b%b%b",b,a,ci,c0,s);
+            $monitor("%b%b%b       %b%b",b,a,ci,c0,s);
             b = i;#10;
             for(j = 0; j < 2; j++)
             begin
-                $monitor("%b%b%b       %b%b%b",b,a,ci,c0,s);
+                $monitor("%b%b%b       %b%b",b,a,ci,c0,s);
                 a = j;#10;
                 for(k = 0; k < 2; k++)
                 begin
-                    $monitor("%b%b%b       %b%b%b",b,a,ci,c0,s);
+                    $monitor("%b%b%b       %b%b",b,a,ci,c0,s);
                     ci = k;#10;
                     assert(ci + b + a == s + c0 * 2);
                 end
