fa57773b3adff1f5971c83bed017db275a9d2929
jakemckenzie <jake314@uw.edu>
2018-05-23 12:10:23 -0700

    init comment

--- UART_Demonstration/TXDriver_testbench.sv
+++ UART_Demonstration/TXDriver_testbench.sv
@@ -1,4 +1,4 @@
-`timescale 1ns/1ns
+`timescale 1s/1s
 
 module TXDriver_testbench();
     //input logic Enable, Reset, TxEmpty, output logic XMitGo, output logic [7:0]TxData
@@ -24,12 +24,12 @@ module TXDriver_testbench();
     initial begin
         Reset = 1;
         //TxEmpty = 0;
-        #100;
+        #1;
         Reset = 0;
         //TxEmpty = 1;
         //$monitor("      %d|      %d|     %b|     %d|     %d|     %c", State, Address, Reset, XMitGo, TxEmpty, TxData);
         $monitor("     %b|     %d|     %d|     %c", Reset, XMitGo, TxEmpty, TxData);
-	    #2708334;//((26.042*10^(-6))*(13*8))/(10^(-9))
+	    #1302;//((26.042*10^(-6))*(13*8))/(10^(-9))
 	    $stop;
     end
 endmodule
